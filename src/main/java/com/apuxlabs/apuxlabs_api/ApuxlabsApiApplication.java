134dc3ad4837b41c382d93f8f131fd4b29cadbd7
nimmalapudi suryateja <suryanimmalapudi@gmail.com>
2026-08-12 09:26:58 +0530

    initial commit

---
+++ src/main/java/com/apuxlabs/apuxlabs_api/ApuxlabsApiApplication.java
@@ -0,0 +1,13 @@
+package com.apuxlabs.apuxlabs_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuxlabsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApuxlabsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apuxlabs/apuxlabs_api/ApuxlabsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apuxlabs.apuxlabs_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApuxlabsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
